66a73d8c1f423bff61f0f6b6cf271e8570d3667a
ManneredPizza <jacopoperoni@MacBook-Pro-di-Jacopo.fritz.box>
2022-10-14 16:07:24 +0200

    adding screenshots

--- mmma/lab3/Runge-Kutta.py
+++ mmma/lab3/Runge-Kutta.py
@@ -1,9 +1,9 @@
 import math
 import matplotlib.pyplot as plt
 
-tau = 1/100
+tau = float(input("Passo Temporale: "))
 w = 2
-N = 1000
+N = round(int(input("Tempo Totale: "))/tau)
 p = [0.2]
 q = [0.3]
 
@@ -16,4 +16,8 @@ for i in range(N):
     p.append(p[i]+tau*f([q[i]+h[0],p[i]+h[1]])[1])
 
 plt.plot(p,q)
+plt.title('Tau: '+str(tau)+' Tempo Totale: '+str(int(N*tau)))
+plt.xlabel('p')
+plt.ylabel('q')
+plt.savefig('runge-kutta-'+str(tau)+'-'+str(int(N*tau))+'.png')
 plt.show()
