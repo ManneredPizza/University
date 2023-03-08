52c69b25d3f985abc1287a975bb801eff43c0cce
ManneredPizza <peroni.jacopo@gmail.com>
2023-03-08 01:51:25 +0100

    more exercise

--- AlgorithmsAndDataStructures/lez02/exercise/es1Main.c
+++ AlgorithmsAndDataStructures/lez02/exercise/es1Main.c
@@ -4,124 +4,115 @@
 
 #define ROW_LENGTH 256
 
+struct parametro {
+  int length;
+  char parametro[ROW_LENGTH];
+};
+
 /* Dichiarazione funzioni secondarie */
-void inizializzaVariabili(int argc, char *argv[], char **listaParametri, int *lenParametri);
-void swapInt(int *a, int *b);
-void swapString(char *a, char *b);
-int* quickSort(int len, int *lenParametri);
+void inizializzaVariabili(int argc, char *argv[], struct parametro *params);
+void copyParametri(struct parametro *a, struct parametro *b);
+void swapParametri(struct parametro *a, struct parametro *b);
+struct parametro* quickSort(int len, struct parametro *params);
 
 
 int main(int argc, char *argv[]) {
 	/* Parte dichiarativa */
-	char **listaParametri = NULL;
-	int *lenParametri = NULL;
-	int *finale = NULL;
-	int i=0;
+	struct parametro *params=NULL;
+	struct parametro *finale=NULL;
+	int i;
 
-	lenParametri = (int*)malloc(sizeof(int) * (argc-1));
-	finale = (int*)malloc(sizeof(int) * (argc-1));
-	listaParametri = (char**)malloc(sizeof(char*) * (argc-1));
-	for( ; i<argc-1; i++)
-		listaParametri[i] = (char*)malloc(sizeof(char) * (argc-1));
+	params = (struct parametro*)malloc(sizeof(struct parametro)*(argc-1));
+	finale = (struct parametro*)malloc(sizeof(struct parametro)*(argc-1));
 
-	inizializzaVariabili(argc, argv, listaParametri, lenParametri);
-	finale = quickSort(argc-1, lenParametri);
+	inizializzaVariabili(argc, argv, params);
+	finale = quickSort(argc-1, params);
 
-	for(i=0; i<argc-1; i++)
-		printf("%d\n", finale[i]);
+    for(i=0; i<argc-1; i++)
+        printf("%s\n", finale[i].parametro);
 
 	return EXIT_SUCCESS;
 }
 
-void inizializzaVariabili(int argc, char *argv[], char **listaParametri, int *lenParametri) {
-	/* alloco dinamicamente gli array con gli input */
+void inizializzaVariabili(int argc, char *argv[], struct parametro *params) {
 	int i=0;
 	for( ; i<argc-1; i++) {
-		/*in ogni stringa inserisco un input*/ 
-		strcpy(listaParametri[i], argv[i+1]); 
+		/*in ogni stringa inserisco un input*/
+		strcpy(params[i].parametro, argv[i+1]);
 		/*inserirsco in ogni cella la lunghezza della stringa nel corrispondete indice*/
-		lenParametri[i] = strlen(argv[i+1]);
+		params[i].length = strlen(argv[i+1]);
 	}
 }
 
-void swapInt(int *a, int *b) {
-	int temp;
-	temp = *a;
-	*a = *b;
-	*b = temp;
+void copyParametri(struct parametro *a, struct parametro *b) {
+    (*a).length = (*b).length;
+    strcpy((*a).parametro, (*b).parametro);
 }
 
-void swapString(char *a, char *b) {
-	char temp[ROW_LENGTH];
-	strcpy(temp, a);
-	strcpy(a, b);
-	strcpy(b, temp);
+void swapParametri(struct parametro *a, struct parametro *b) {
+    struct parametro temp;
+    copyParametri(&temp, a);
+    copyParametri(a,b);
+    copyParametri(b,&temp);
 }
 
-/* Da testare */
-int* quickSort(int len, int *lenParametri) {
-	/* Dovrò capire poi come ordinare la listaParametri */
-	int pivot;
-	int *parte1Int = NULL;
-	int *parte2Int = NULL;
-	int *final1 = NULL;
-	int *final2 = NULL;
-	int *finale = NULL;
+/* variante quicksort per struct con un int da ordinare */
+struct parametro* quickSort(int len,  struct parametro *params) {
+	struct parametro pivot;
+	struct parametro* parte1 = NULL;
+	struct parametro* parte2 = NULL;
+	struct parametro* final1 = NULL;
+	struct parametro* final2 = NULL;
+	struct parametro* finale = NULL;
 	int i = 1;
 	int j = 0;
 	int k = 0;
 
-	/* Se len < 3 */
-	/* len = 0 ritorno null */
-	/* len = 1 ritorno */
-	/* len = 2 ordino */
 	if(len == 0)
 		return NULL;
 	if(len == 1)
-		return lenParametri;
+		return params;
 	if(len == 2) {
-		if(lenParametri[0] <= lenParametri[1])
-			return lenParametri;
-		swapInt(&lenParametri[0], &lenParametri[1]);
-		return lenParametri;
+		if(params[0].length <= params[1].length)
+			return params;
+		swapParametri(&params[0], &params[1]);
+		return params;
 	}
 
-	finale = (int*)malloc(sizeof(int)*len);
+	finale = (struct parametro*)malloc(sizeof(struct parametro)*len);
 
-	pivot = lenParametri[0];
-	parte1Int = (int*)malloc(sizeof(int));
-	final1 = (int*)malloc(sizeof(int));
-	parte2Int = (int*)malloc(sizeof(int));
-	final2 = (int*)malloc(sizeof(int));
+    copyParametri(&pivot, &params[0]);
+
+	parte1 = (struct parametro*)malloc(sizeof(struct parametro));
+	final1 = (struct parametro*)malloc(sizeof(struct parametro));
+	parte2 = (struct parametro*)malloc(sizeof(struct parametro));
+	final2 = (struct parametro*)malloc(sizeof(struct parametro));
 
 	for( ;i<len; i++) {
-		if(lenParametri[i] <= pivot) {
+		if(params[i].length <= pivot.length) {
 			if(j>0)
-				parte1Int = (int*)realloc(parte1Int, sizeof(int)*(j+1));
-				final1 = (int*)realloc(final1, sizeof(int)*(j+1));
-			parte1Int[j] = lenParametri[i];
+				parte1 = (struct parametro*)realloc(parte1, sizeof(struct parametro)*(j+1));
+				final1 = (struct parametro*)realloc(final1, sizeof(struct parametro)*(j+1));
+            copyParametri(&parte1[j], &params[i]);
 			j++;
 		}
 		else {
 			if(k>0)
-				parte2Int = (int*)realloc(parte2Int, sizeof(int)*(k+1));
-				final2 = (int*)realloc(final2, sizeof(int)*(k+1));
-			parte2Int[j] = lenParametri[i];
+				parte2 = (struct parametro*)realloc(parte2, sizeof(struct parametro)*(k+1));
+				final2 = (struct parametro*)realloc(final2, sizeof(struct parametro)*(k+1));
+            copyParametri(&parte2[k], &params[i]);
 			k++;
 		}
 	}
 
-	final1 = quickSort(j, parte1Int);
-	final2 = quickSort(k ,parte2Int);
+	final1 = quickSort(j, parte1);
+	final2 = quickSort(k, parte2);
 
 	for(i=0; i<j; i++)
-		finale[i] = final1[i];
-	finale[i] = pivot;
-	i++;
-	for( ; i<k+j; i++)
-		finale[i] = final2[i-j-1];
-	/* deve ritornare quickSort(j, parte1Int) || pivot || quickSort(k, parte2Int) */
-	/* la contcatenazione deve tenere conto della possibilità che sia vuoto */
+        copyParametri(&finale[i], &final1[i]);
+    copyParametri(&finale[j], &pivot);
+	for(i=0; i<k; i++)
+        copyParametri(&finale[i+j+1], &final2[i]);
 
 	return finale;
 }
