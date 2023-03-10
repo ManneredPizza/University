82820b579069697611ae6e86f7e90154a55d667c
ManneredPizza <jacopoperoni@MacBook-Pro-di-Jacopo.fritz.box>
2023-03-10 19:24:19 +0100

    lab04 finished

--- AlgorithmsAndDataStructures/lez04/scacchiera.c
+++ AlgorithmsAndDataStructures/lez04/scacchiera.c
@@ -64,21 +64,19 @@ void stampaStatoScacchiera(Scacchiera S) {
         fprintf(stdout, "Pareggio\n");
 }
 
-int numeroCelleValideIntorno(int riga, int colonna, Scacchiera S) {
-    /* capire come controllare in maniera efficiente l'intorno dato un punto di partenza valido */
-
-    return 0;
-}
-
 boolean verificaStallo(Scacchiera S, char giocatore) {
     int i;
-    int j;
+    int j; 
+    /* non serve a nulla */
+    validaMossa vm;
+
+    vm.length = 0;
 
     for(i=0; i<BOARD_SIZE; i++) {
         for(j=0; j<BOARD_SIZE; j++) {
-            if(S[i][j] == LIBERO)
+            if(S[i][j] != LIBERO)
                 continue;
-            if(numeroCelleValideIntorno(i, j, S) > 0)
+            if(verificaValiditaMossa(i, j, giocatore, &vm, S) == TRUE)
                 return FALSE;
         }
     }
@@ -101,22 +99,86 @@ posizione daScacchieraACoppia(char *posizioneS) {
     return p;
 }
 
-boolean verificaValiditaMossa(int riga, int colonna, Scacchiera S) {
+int verificaDirezione(int rigaOffset, int colonnaOffset, int riga, int colonna, char giocatore, Scacchiera S) {
+    int i=rigaOffset;
+    int j=colonnaOffset;
+    int counter=0;
+    boolean found=FALSE;
+
+    /* con questo controllo posso usare lo stesso codice per ogni punto, anche quelli al bordo */
+    while(riga+i>=0 && riga+i<BOARD_SIZE && colonna+j<BOARD_SIZE && colonna+j>=0) {
+        if(S[riga+i][colonna+j] == LIBERO)
+            break;
+        if(S[riga+i][colonna+j] == giocatore) {
+            found = TRUE;
+            break;
+        }
+        counter++;
+        i+=rigaOffset;
+        j+=colonnaOffset;
+    }
+
+    if(found==TRUE)
+        return counter;
+
+    /* anche se incontro un po' di non-giocatore ritorno zero se non trovo giocatore ma un libero */
+    return 0;
+}  
+
+boolean verificaValiditaMossa(int riga, int colonna, char giocatore, validaMossa *vm, Scacchiera S) {
     /* capire come controllare la validita' di una mossa */
     /* Se la sequenza di mosse non `e corretta, si deve stampare a video la mossa non lecita e il giocatore a cui `e stata attribuita */
+    int i;
+    int j;
+    int counter = 0;
+    int parziale;
 
-    return TRUE;
+    if(S[riga][colonna] != LIBERO)
+        return FALSE;
+
+    for(i=-1; i<2; i++) {
+        for(j=-1; j<2; j++) {
+            if(i!=0 || j!=0) {
+                parziale = verificaDirezione(i, j, riga, colonna, giocatore, S);
+                if(parziale != 0) {
+                    (*vm).xOffset[vm->length] = i;
+                    (*vm).yOffset[vm->length] = j;
+                    vm->length++;
+                }
+                counter += parziale;
+            }
+        }
+    }
+        
+    if(counter != 0)
+        return TRUE;
+    return FALSE;
 }
 
 void eseguiMossa(char *posizioneS, char giocatore, Scacchiera S) {
     posizione p;
+    validaMossa vm;
+    int i;
+    int k;
+
+    vm.length = 0;
 
     p = daScacchieraACoppia(posizioneS);
 
-    if(verificaValiditaMossa(p.riga, p.colonna, S) == FALSE) {
+    if(verificaValiditaMossa(p.riga, p.colonna, giocatore, &vm, S) == FALSE) {
         fprintf(stderr, "Mossa %s eseguita da %c non e' valida\n", posizioneS, giocatore);
         exit(EXIT_FAILURE);
     }
 
+    /* seg fault sicuro, il vettore dinamico non può essere passato */
+
     S[p.riga][p.colonna] = giocatore;
+
+    for(i=0; i<vm.length; i++) {
+        k=1;
+        while(S[p.riga+vm.xOffset[i]*k][p.colonna+vm.yOffset[i]*k] != giocatore) {
+            S[p.riga+vm.xOffset[i]*k][p.colonna+vm.yOffset[i]*k] = giocatore;
+            k++;
+        }
+    }
 }
--- AlgorithmsAndDataStructures/lez04/scacchiera.h
+++ AlgorithmsAndDataStructures/lez04/scacchiera.h
@@ -12,6 +12,8 @@ typedef int boolean;
 
 #define MOVE_LENGTH 2
 
+#define NEIGH_SIZE 8
+
 /* prototipi delle funzioni secondarie */
 typedef char Scacchiera[BOARD_SIZE][BOARD_SIZE];
 
@@ -20,6 +22,13 @@ typedef struct {
     int riga;
     int colonna;
 } posizione;
+
+/* tiene traccia delle direzioni in cui si devono effettuare le modifiche */
+typedef struct {
+    int xOffset[NEIGH_SIZE];
+    int yOffset[NEIGH_SIZE];
+    int length;
+} validaMossa;
 
 /* inizializza la scacchiera a solo simboli LIBERO tranne le condizioni di partenza centrali */
 void inizializzaScacchiera(Scacchiera S);
@@ -30,17 +39,17 @@ void stampaScacchiera(Scacchiera S);
 /* stampa numero dei NERI numero dei BIANCHI ed il vincitore */
 void stampaStatoScacchiera(Scacchiera S);
 
-/* prende un punto di un giocatore e conta in quanti punti puo' giocare */
-int numeroCelleValideIntorno(int riga, int colonna, Scacchiera S);
-
 /* verifica stallo data la scacchiera ed il giocatore */
 boolean verificaStallo(Scacchiera S, char giocatore);
 
 /* prende posizioni del tipo a5 e restituisce la coppia (0,4) */
 posizione daScacchieraACoppia(char *posizioneS);
 
+/* dato un vettore verso cui muoversi conta il numero di pedine del colore opposto presenti prima di quella del colore del giocatore */
+int verificaDirezione(int rigaOffset, int colonnaOffset, int riga, int colonna, char giocatore, Scacchiera S);
+
 /* verifica che la mossa richiesta possa essere eseguita */
-boolean verificaValiditaMossa(int riga, int colonna, Scacchiera S);
+boolean verificaValiditaMossa(int riga, int colonna, char giocatore, validaMossa *vm, Scacchiera S);
 
 /* inserisce il giocatore nella posizione richiesta (non ne controlla la validita') */
 void eseguiMossa(char *posizioneS, char giocatore, Scacchiera S);
